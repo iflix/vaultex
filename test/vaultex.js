abb10708e49609bc574885161c6c550d88b4c378
Adam Brady <adam@boxxen.org>
2016-03-26 00:15:45 +1100

    HA, Seal tests

--- test/vaultex.js
+++ test/vaultex.js
@@ -1,4 +1,4 @@
-/*global describe, before, after, it*/
+/*global describe, before, after, it, xit*/
 import assert from 'assert'
 
 import spawnDevVault from 'spawn-dev-vault'
@@ -271,6 +271,100 @@ describe('Vaultex', function () {
           })
         })
       })
+    })
+  })
+  describe('HA', function () {
+    describe('getLeader', function () {
+      it('should return leader details', function (done) {
+        spawnVault(true, function (vaultex) {
+          vaultex.ha.getLeader(function (err, leader) {
+            assert.ifError(err)
+
+            assert.deepEqual(leader, {
+              ha_enabled: false,
+              is_self: false,
+              leader_address: ''
+            })
+
+            vaultex.kill(done)
+          })
+        })
+      })
+    })
+  })
+  describe('Seal', function () {
+    let sharedVaultex = null
+
+    before(function (done) {
+      spawnVault(true, function (vaultex, vaultData) {
+        sharedVaultex = vaultex
+        sharedVaultex.vaultData = vaultData
+        done()
+      })
+    })
+
+    after(function (done) {
+      sharedVaultex.kill(done)
+    })
+
+    describe('status', function () {
+      it('should get status of vault seal', function (done) {
+        sharedVaultex.seal.status(function (err, status) {
+          assert.ifError(err)
+
+          assert.deepEqual(status, {
+            sealed: false,
+            t: 1,
+            n: 1,
+            progress: 0
+          })
+
+          done()
+        })
+      })
+    })
+
+    describe('seal', function () {
+      it('should seal vault', function (done) {
+        sharedVaultex.seal.seal(function (err) {
+          assert.ifError(err)
+
+          sharedVaultex.seal.status(function (err, status) {
+            assert.ifError(err)
+
+            assert.deepEqual(status, {
+              sealed: true,
+              t: 1,
+              n: 1,
+              progress: 0
+            })
+
+            done()
+          })
+        })
+      })
+    })
+
+    describe('unseal', function () {
+      it('should unseal vault', function (done) {
+        sharedVaultex.seal.unseal(sharedVaultex.vaultData.key, function (err) {
+          assert.ifError(err)
+
+          sharedVaultex.seal.status(function (err, status) {
+            assert.ifError(err)
+
+            assert.deepEqual(status, {
+              sealed: false,
+              t: 1,
+              n: 1,
+              progress: 0
+            })
+
+            done()
+          })
+        })
+      })
+      xit('should partially unseal vault when required keys n > 1')
     })
   })
 })
